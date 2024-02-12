1af5c58970631e24a6aee67ba50816e86d83a65b
Joel Mora <joeljmorab@gmail.com>
2024-02-12 15:27:05 -0300

    servicio fetch creado

---
+++ src/views/IndexView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script lang="ts" setup>
+import PostServices from "@/services/PostServices.ts"
+
+const services = new PostServices
+
+
+</script>
+
+<style scoped>
+</style>
